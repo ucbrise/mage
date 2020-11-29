2148946c224bd2b4d5aa7f388aa474230fd9b417
Sam Kumar <samkumar99@gmail.com>
2020-11-29 00:44:44 -0800

    Update benchmarking script

--- bench.sh
+++ bench.sh
@@ -2,37 +2,56 @@
 
 set -x
 
-IP=$1
-CMD=$2
-port=54322
-
+SWAP_DEVICE=$1
+ROLE=$2
 
 function invoke {
-	if [ $CMD == "garble" ]
+	if [ $ROLE == "garbler" ]
 	then
-		sleep 15
+		sleep 160
+	else
+		sleep 125
 	fi
 
 	sudo free
 	sudo sync
 	echo 3 | sudo tee /proc/sys/vm/drop_caches
 	sudo free
-	echo $port
-	$2 ./bin/mage $CMD $1/aspirin_${i} ${IP}:${port} > logs/aspirin_${3}_${i}_${CMD}.log
+	$1 ./mage $2 $3 $4 0 $5 > ~/work/logs/${5}_${6}_${7}.log
+	if [ $ROLE == "garbler" ]
+	then
+		diff ${5}_0.output ${5}_0.expected > ~/work/logs/${5}_${6}_${7}.result
+	fi
 }
 
-#for i in 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432 67108864
-for i in 131072 33554432
-do
-	echo $i
+function run_benchmark {
+	# $1 is protocol (e.g., halfgates)
+	# $2 is the problem name
+	# $3 is the problem size
+	# $4 is the number of trials
+	for trial in $(seq $4)
+	do
+		sudo swapoff -a
+		./example_input $2 $3 1
+		./planner $2 ../config_unbounded.yaml $ROLE 0 $3
+		sudo swapon ${SWAP_DEVICE}
+		invoke "sudo cgexec -g memory:memprog" $1 ../config_unbounded.yaml $ROLE ${2}_${3} os t$trial
+		sudo swapoff -a
+		invoke "sudo" $1 ../config_unbounded.yaml $ROLE ${2}_${3} unbounded t$trial
+		./planner $2 ../config.yaml $ROLE 0 $3
+		invoke "sudo cgexec -g memory:memprog" $1 ../config.yaml $ROLE ${2}_${3} mage t$trial
+	done
+}
 
-	sudo swapoff -a
-	sudo swapon /dev/nvme0n1p3
-	port=$((port+1))
-	invoke ~/work/files/unbounded "sudo cgexec -g memory:1gb" os_t1
-	sudo swapoff -a
-	port=$((port+1))
-	invoke ~/work/files/unbounded "sudo" unbounded_t1
-	port=$((port+1))
-	invoke ~/work/files/1gb "sudo cgexec -g memory:1gb" mage_t1
-done
+mkdir -p ~/work/logs
+
+run_benchmark halfgates merge_sorted 1048576 10
+run_benchmark halfgates full_sort 1048576 10
+run_benchmark halfgates loop_join 2048 10
+run_benchmark halfgates matrix_vector_mult 8192 10
+run_benchmark halfgates binary_fc_layer 16384 10
+# run_benchmark halfgates merge_sorted 1024 3
+# run_benchmark halfgates full_sort 1024 3
+# run_benchmark halfgates loop_join 256 3
+# run_benchmark halfgates matrix_vector_mult 256 3
+# run_benchmark halfgates binary_fc_layer 512 3
